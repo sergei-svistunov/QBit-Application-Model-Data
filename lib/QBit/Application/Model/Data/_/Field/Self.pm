e876683c996e3d0dcd08eeea228d9d4c8e39f97f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-11-19 21:58:29 +0800

    Delete lib/QBit/Application/Model directory

--- lib/QBit/Application/Model/Data/_/Field/Self.pm
+++
@@ -1,7 +0,0 @@
-package QBit::Application::Model::Data::_::Field::Self;
-
-use qbit;
-
-use base qw(QBit::Application::Model::Data::_::Field);
-
-TRUE;
--- lib/QBit/Application/Model/Data/_/Field/number.pm
+++
@@ -1,7 +0,0 @@
-package QBit::Application::Model::Data::_::Field::number;
-
-use qbit;
-
-use base qw(QBit::Application::Model::Data::_::Field::Self);
-
-TRUE;
